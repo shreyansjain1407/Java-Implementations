ebac92448a9ab550400a23772132f82e4f801d02
Shreyans Jain <shreyansjain1407@gmail.com>
2024-04-10 19:49:15 -0700

    Arrays Begin

---
+++ LeetCode_Prep/src/SetMatrixZeroes.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class SetMatrixZeroes {
+    public void setZeroes(int[][] matrix) {
+        Set<Integer> rows = new HashSet<>();
+        Set<Integer> cols = new HashSet<>();
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j] == 0) {
+                    rows.add(i);
+                    cols.add(j);
+                }
+            }
+        }
+
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                if(rows.contains(i) || cols.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        SetMatrixZeroes x = new SetMatrixZeroes();
+//        [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+        int[][] arr = new int[][]{{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+        x.setZeroes(arr);
+    }
+}
